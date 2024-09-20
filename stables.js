930daa0c0d45109f7385fbd55c0ecd2c709ceb18
BlankBozo <donaldwongplays@gmail.com>
2024-09-20 15:40:22 -0700

    initialize stables

---
+++ stables.js
@@ -0,0 +1,11 @@
+let horseName = "PinkleDinkyFam";
+console.log(horseName);
+horseName = "PDF";
+console.log(horseName);
+
+let horseAge = "420";
+console.log(horseAge);
+
+let isHorseInside = true;
+console.log(isHorseInside)
+
