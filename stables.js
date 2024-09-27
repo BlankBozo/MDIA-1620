92cf548b9fedd42055b01156f88803717436416f
BlankBozo <donaldwongplays@gmail.com>
2024-09-27 15:20:46 -0700

    Adding more info to stables

--- stables.js
+++ stables.js
@@ -3,9 +3,21 @@ console.log(horseName);
 horseName = "PDF";
 console.log(horseName);
 
-let horseAge = "420";
+let horseAge = "80082";
 console.log(horseAge);
 
 let isHorseInside = true;
-console.log(isHorseInside)
+console.log(isHorseInside);
 
+// LESSON-4 Code
+console.log(typeof 80082);
+const STABLE_MONTHLY_FEE = 1492;
+console.log(STABLE_MONTHLY_FEE);
+
+console.log(STABLE_MONTHLY_FEE * 3);
+console.log(STABLE_MONTHLY_FEE + 100);
+console.log(20/2);
+console.log("Bob" + " Ross");
+console.log(2 + "high") // type coercion
+console.log("The fee for stable is " + STABLE_MONTHLY_FEE);
+console.log(`This is ${horseName} and he is a silly lil fella. It costs ${STABLE_MONTHLY_FEE} to board him.`);
