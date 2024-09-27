155c163af6cd56f2abf843fb4140cb3ad7242e48
BlankBozo <donaldwongplays@gmail.com>
2024-09-27 16:14:22 -0700

    Created horse nickname

--- stables.js
+++ stables.js
@@ -1,7 +1,5 @@
 let horseName = "PinkleDinkyFam";
 console.log(horseName);
-horseName = "PDF";
-console.log(horseName);
 
 let horseAge = "80082";
 console.log(horseAge);
@@ -21,3 +19,10 @@ console.log("Bob" + " Ross");
 console.log(2 + "high") // type coercion
 console.log("The fee for stable is " + STABLE_MONTHLY_FEE);
 console.log(`This is ${horseName} and he is a silly lil fella. It costs ${STABLE_MONTHLY_FEE} to board him.`);
+
+//Lab 4 work
+
+let horseNickname = "PDF";
+console.log(horseNickname);
+console.log(`This is ${horseNickname} and this is his silly nickname. He likes our specific stable, which costs ${STABLE_MONTHLY_FEE}`)
+console.log(`If you stay with us for three months,${((STABLE_MONTHLY_FEE * 3 *.1))} will be deducted from your total costs.`)
